dfddb1c682a76e663c91d04f1b2f6693d0f01d69
AlexMI <69844256+Alex071004@users.noreply.github.com>
2026-02-26 13:42:13 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+DAYS = 90
+TREND_DAYS = 60
+CARS_COUNT = 100
+AVG_OPENINGS_PER_CAR_PER_DAY = 20
+TOTAL_AVG_OPENINGS = CARS_COUNT * AVG_OPENINGS_PER_CAR_PER_DAY
+
+TOUCHPOINTS = ['button', 'svp', 'va', 'app']
+TOUCHPOINT_NAMES_RU = {
+    'button': 'Кнопка',
+    'svp': 'СВП (руль)',
+    'app': 'Приложение',
+    'va': 'VA'
+}
