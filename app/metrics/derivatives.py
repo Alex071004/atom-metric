380e9295625a92da122b04b90eef0982748523dd
AlexMI <69844256+Alex071004@users.noreply.github.com>
2026-02-26 15:42:29 +0300

    Update derivatives.py

--- app/metrics/derivatives.py
+++ app/metrics/derivatives.py
@@ -1,72 +1,81 @@
-"""Вычисление производных и углов наклона."""
+"""Вычисление производных для дискретных данных."""
 import numpy as np
 import pandas as pd
 from typing import Dict, List, Tuple, Optional
-from scipy import interpolate
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import config
-from .approximation import DataApproximator
 
 class DerivativeCalculator:
     """
     Калькулятор производных для дискретных функций.
-    Возвращает тангенс угла наклона (Δy/Δx), а не градусы.
+    Производная = (y[i+1] - y[i]) / (x[i+1] - x[i])
     """
     
-    def __init__(self, use_smoothing: bool = True, interpolation_points: int = 10):
+    def __init__(self, use_smoothing: bool = False, interpolation_points: int = 5):
         self.use_smoothing = use_smoothing
-        # Для сглаживания используем адаптивный параметр
-        self.approximator = DataApproximator(smoothing_factor=None, 
-                                            interpolation_points=interpolation_points)
+        self.interpolation_points = interpolation_points
     
     def calculate_derivatives(self, values: List[int], dates: Optional[List] = None) -> Tuple[np.ndarray, np.ndarray]:
         """
-        Вычисление производных (тангенса угла наклона).
-        Возвращает (x, tangent_values)
+        Вычисление производных методом конечных разностей.
+        Возвращает (x_centers, derivative_values)
+        где x_centers - средние точки между днями
         """
-        if dates is None:
-            x = np.arange(len(values))
-        else:
-            x = np.arange(len(dates))
+        x = np.arange(len(values))
+        y = np.array(values) / config.CARS_COUNT  # на одну машину
         
-        if self.use_smoothing:
-            return self.approximator.calculate_smoothed_derivative(x, values, per_car=True)
-        else:
-            return self.approximator.calculate_derivative_tangent(x, values, per_car=True)
+        # Вычисляем производные в средних точках между днями
+        x_centers = (x[:-1] + x[1:]) / 2
+        derivatives = (y[1:] - y[:-1]) / (x[1:] - x[:-1])
+        
+        return x_centers, derivatives
     
     def calculate_angles_at_points(self, values: List[int], dates: Optional[List] = None) -> List[float]:
         """
-        Вычисление тангенсов только в исходных точках.
+        Вычисление производных в исходных точках (для обратной совместимости)
         """
         x = np.arange(len(values))
+        y = np.array(values) / config.CARS_COUNT
         
-        if self.use_smoothing:
-            x_dense, tangents_dense = self.approximator.calculate_smoothed_derivative(x, values, per_car=True)
-            # Интерполируем обратно в исходные точки
-            f = interpolate.interp1d(x_dense, tangents_dense, kind='linear', 
-                                    bounds_error=False, fill_value='extrapolate')
-            tangents = f(x)
-            return tangents.tolist()
-        else:
-            _, tangents = self.approximator.calculate_derivative_tangent(x, values, per_car=True)
-            return tangents.tolist()
+        # Центральные разности для внутренних точек
+        derivatives = np.zeros(len(values))
+        
+        if len(values) >= 2:
+            # Левая граница - правая разность
+            derivatives[0] = (y[1] - y[0]) / (x[1] - x[0])
+            
+            # Правые границы - левая разность
+            derivatives[-1] = (y[-1] - y[-2]) / (x[-1] - x[-2])
+            
+            # Центральные разности для внутренних точек
+            for i in range(1, len(values)-1):
+                derivatives[i] = (y[i+1] - y[i-1]) / (x[i+1] - x[i-1])
+        
+        return derivatives.tolist()
     
-    def sum_of_angles(self, data: pd.DataFrame) -> pd.Series:
+    def sum_of_derivatives(self, data: pd.DataFrame) -> pd.Series:
         """
-        Сумма тангенсов углов наклона всех тачпоинтов.
+        Сумма производных всех тачпоинтов.
         """
-        x = np.arange(len(data))
-        total_tangents = np.zeros(len(x))
+        total_derivatives = None
         
         for tp in config.TOUCHPOINTS:
-            _, tangents = self.calculate_derivatives(data[tp].tolist(), data['date'].tolist())
-            # Интерполируем обратно в исходные точки
-            f = interpolate.interp1d(np.arange(len(tangents)), tangents, kind='linear',
-                                    bounds_error=False, fill_value='extrapolate')
-            tangents_at_x = f(x)
-            total_tangents += tangents_at_x
+            x_centers, derivs = self.calculate_derivatives(data[tp].tolist(), data['date'].tolist())
+            
+            if total_derivatives is None:
+                total_derivatives = derivs
+            else:
+                # Интерполируем до одинаковой длины если нужно
+                if len(derivs) != len(total_derivatives):
+                    # Берем минимальную длину
+                    min_len = min(len(derivs), len(total_derivatives))
+                    total_derivatives = total_derivatives[:min_len] + derivs[:min_len]
+                else:
+                    total_derivatives += derivs
         
-        return pd.Series(total_tangents, index=data.index)
+        # Создаем Series с индексами как у исходных данных (приблизительно)
+        result = pd.Series(total_derivatives, index=range(len(total_derivatives)))
+        return result
