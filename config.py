4f3d1d902efbe725adb470e4ca588a249f4ec354
AlexMI <69844256+Alex071004@users.noreply.github.com>
2026-02-26 13:58:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
 
-START_DATE = datetime.now().date() - timedelta(days=DAYS-1)
-
 DAYS = 90
 TREND_DAYS = 60
 CARS_COUNT = 100
 AVG_OPENINGS_PER_CAR_PER_DAY = 20
 TOTAL_AVG_OPENINGS = CARS_COUNT * AVG_OPENINGS_PER_CAR_PER_DAY
+
+START_DATE = datetime.now().date() - timedelta(days=DAYS-1)
 
 TOUCHPOINTS = ['button', 'svp', 'va', 'app']
 TOUCHPOINT_NAMES_RU = {
