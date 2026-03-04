a3d791d698957934f94cea2d9a3c1c4ad8e5466e
AlexMI <69844256+Alex071004@users.noreply.github.com>
2026-03-04 13:47:22 +0300

    Update supabase-config.js

--- frontend/supabase/supabase-config.js
+++ frontend/supabase/supabase-config.js
@@ -1,1 +1,8 @@
+// supabase/supabase-config.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
+// Эти данные вы получили в Supabase: Settings → API
+const supabaseUrl = 'https://sceytpejjhrvylvnmuwe.supabase.co'  // замените на ваш URL
+const supabaseAnonKey = 'sb_publishable_emjXR7LILlp1kk_QXSWVmw_1C7p-dtx'               // замените на ваш anon key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
